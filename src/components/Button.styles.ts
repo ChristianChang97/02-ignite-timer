689557253744dff231e4c36a7909fe3b7924fe01
Christian Chang <christianchangthomaz@gmail.com>
2023-03-19 01:20:09 -0300

    feat: Set Themes

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -17,7 +17,12 @@ const buttonVariants = {
 export const ButtonContainer = styled.button<ButtonContainerProps>`
     width: 100px;
     height: 40px;
+    border-radius: 4px;
+    border: 0;
+    margin: 8px;
+    background-color: ${props => props.theme.primary};
+    color: ${props => props.theme.white};
 
-    ${props => `background-color: ${buttonVariants[props.variant]}`}
+    // ${props => `background-color: ${buttonVariants[props.variant]}`}
 `
 
